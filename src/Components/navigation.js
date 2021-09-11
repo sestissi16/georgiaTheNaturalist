bc0ae3541fbe572b745c6cd314614156cd0ee8d4
sestissi16 <sestissi16@earlham.edu>
2021-09-11 15:35:21 -0700

    changed nav href

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -9,15 +9,15 @@ class Navigation extends Component {
         const { location } = this.props;
         return (
             <Navbar collapseOnSelect expand="xl" variant={ variant } className="Nav">
-                <NavbarBrand href="/" id="navBrand">Georgia The Naturalist</NavbarBrand>
+                <NavbarBrand href="/georgiaTheNaturalist/" id="navBrand">Georgia The Naturalist</NavbarBrand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav activeKey={location.pathname}>
-                        <Nav.Link href="/AboutMe" activeStyle={{ color: 'red' }} >About Me</Nav.Link>
-                        <Nav.Link href="/Programming" activeStyle={{ color: 'red' }}>Programming</Nav.Link>
-                        <Nav.Link href="/Photos" activeStyle={{ color: 'red' }}>Photos</Nav.Link>
-                        <Nav.Link href="/Pricing" activeStyle={{ color: 'red' }}>Pricing</Nav.Link>
-                        <Nav.Link href="/Contact" activeStyle={{ color: 'red' }}>Contact - Scheduling</Nav.Link>
+                        <Nav.Link href="/georgiaTheNaturalist/AboutMe" activeStyle={{ color: 'red' }} >About Me</Nav.Link>
+                        <Nav.Link href="/georgiaTheNaturalist/Programming" activeStyle={{ color: 'red' }}>Programming</Nav.Link>
+                        <Nav.Link href="/georgiaTheNaturalist/Photos" activeStyle={{ color: 'red' }}>Photos</Nav.Link>
+                        <Nav.Link href="/georgiaTheNaturalist/Pricing" activeStyle={{ color: 'red' }}>Pricing</Nav.Link>
+                        <Nav.Link href="/georgiaTheNaturalist/Contact" activeStyle={{ color: 'red' }}>Contact - Scheduling</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
